db109329d69c09b99503a224061cd13c0c387353
caceoli <caceoli.pir@hotmail.com>
2026-05-04 21:11:19 -0300

    Ajuste abertura de banco de dados main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from feedin import app, databaseexit
+from feedin import app, database
 
 with app.app_context():
     database.create_all()
+    print("Banco de dados verificado com sucesso!")
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run()
